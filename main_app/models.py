9fb1597759521b2b1a6f902e16d110519420aac8
CinqueaS <cinquea.stafford@gmail.com>
2024-12-10 16:45:27 -0500

    creating models for non dummy data, Have to make a chapters model to properly show the list of chapters

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Story(models.Model):
+    title = models.CharField(max_length= 100)
+    chapters = models.IntegerField()
+    synopsis = models.TextField(max_length = 500)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("story_detail", kwargs={"story_id": self.id})
+    
