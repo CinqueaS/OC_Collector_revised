092e99720efe779e1fe1dfb4f7bf53006b84eb35
CinqueaS <cinquea.stafford@gmail.com>
2024-12-10 15:36:04 -0500

    using dummy data to test routes at the moment, static images are being added and preparing to make detail page for stories

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
 
+class Story:
+    def __init__(self, title, chapters, synopsis):
+        self.title = title
+        self.chapters = chapters
+        self.synopsis = synopsis
+
+stories = [
+    Story('Dreamless', 12, "Dreamless is a fantasy story featuring the adventures of the greatest bounty hunter in the land, the Crimson Samurai - Haina Jina. Haina faces peril at every turn as he navigates the threat of a once thought extinguished villain's return"),
+    Story('WitchKnight', 5, "WitchKnight synopsis"),
+    Story('THB', 5, "THB synopsis")
+]
+
 def home(request):
     return render(request, 'home.html')
+
+def story_index(request):
+    return render(request, 'stories.html', {'stories': stories})
