bf841305d65fd655fb4e36c5f433e0afca493526
CinqueaS <cinquea.stafford@gmail.com>
2024-12-11 16:02:42 -0500

    Full CRUD on stories created

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('stories/', views.story_index, name='stories'),
     path('stories/<int:story_id>/', views.story_detail, name='story-detail' ),
-    path('stories/create/', views.StoryCreate.as_view(), name='story-create')
+    path('stories/create/', views.StoryCreate.as_view(), name='story-create'),
+    path('stories/<int:pk>/update/', views.StoryUpdate.as_view(), name='story-update'),
+    path('stories/<int:pk>/delete/', views.StoryDelete.as_view(), name='story-delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Story 
 
 def home(request):
@@ -16,5 +16,13 @@ def story_detail(request, story_id):
 
 class StoryCreate(CreateView):
     model = Story
-    fields = '__all__' 
+    fields = ['title', 'chapters', 'synopsis'] 
     success_url= '/stories/'
+
+class StoryUpdate(UpdateView):
+    model = Story
+    fields = ['title', 'chapters', 'synopsis']
+
+class StoryDelete(DeleteView):
+    model = Story
+    success_url = '/stories/'
