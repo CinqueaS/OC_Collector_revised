54f4f96df8a9a39d1af7991022f98bf3ebcc85cb
CinqueaS <cinquea.stafford@gmail.com>
2024-12-13 14:33:53 -0500

    user auth finished, some conditional logic for rendering I want to figure out before making the about page

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Story 
 from .forms import ChapterForm
 
@@ -17,6 +20,7 @@ def story_detail(request, story_id):
     chapter_form = ChapterForm()
     return render(request, 'stories/detail.html', {'story': story, 'chapter_form': chapter_form})
 
+@login_required
 def add_chapter(request, story_id):
 
     form = ChapterForm(request.POST)
@@ -28,9 +32,21 @@ def add_chapter(request, story_id):
         new_chapter.save()
     return redirect('story-detail', story_id=story_id)
 
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('cat-index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
 
-
-class StoryCreate(CreateView):
+class StoryCreate(LoginRequiredMixin, CreateView):
     model = Story
     fields = ['title', 'chapters', 'synopsis'] 
     success_url= '/stories/'
@@ -39,10 +55,10 @@ class StoryCreate(CreateView):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
-class StoryUpdate(UpdateView):
+class StoryUpdate(LoginRequiredMixin, UpdateView):
     model = Story
     fields = ['title', 'chapters', 'synopsis']
 
-class StoryDelete(DeleteView):
+class StoryDelete(LoginRequiredMixin, DeleteView):
     model = Story
     success_url = '/stories/'
