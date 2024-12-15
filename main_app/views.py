591e948ab74c9ad31d7985f9c30aed7f96d32930
CinqueaS <cinquea.stafford@gmail.com>
2024-12-15 12:15:03 -0500

    fixed a redirect bug

--- main_app/views.py
+++ main_app/views.py
@@ -42,7 +42,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('cat-index')
+            return redirect('stories')
         else:
             error_message = 'Invalid sign up - try again'
     form = UserCreationForm()
